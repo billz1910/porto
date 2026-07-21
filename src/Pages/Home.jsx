b749a4b7533d98299ef98479493794f2bb1e0eb6
billz1910 <159798833+billz1910@users.noreply.github.com>
2026-07-21 14:40:17 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -24,14 +24,14 @@ const MainTitle = memo(() => (
       <span className="relative inline-block">
         <span className="absolute -inset-2 bg-gradient-to-r from-[#6366f1] to-[#a855f7] blur-2xl opacity-20"></span>
         <span className="relative bg-gradient-to-r from-white via-blue-100 to-purple-200 bg-clip-text text-transparent">
-          Frontend
+          Cyber Security
         </span>
       </span>
       <br />
       <span className="relative inline-block mt-2">
         <span className="absolute -inset-2 bg-gradient-to-r from-[#6366f1] to-[#a855f7] blur-2xl opacity-20"></span>
         <span className="relative bg-gradient-to-r from-[#6366f1] to-[#a855f7] bg-clip-text text-transparent">
-          Developer
+          Analyst
         </span>
       </span>
     </h1>
@@ -76,8 +76,8 @@ const SocialLink = memo(({ icon: Icon, link, label }) => (
 const TYPING_SPEED = 100;
 const ERASING_SPEED = 50;
 const PAUSE_DURATION = 2000;
-const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
-const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
+const WORDS = ["Bug Bounty", "Analyst"];
+const TECH_STACK = ["React", "Javascript", "Php", "Python"];
 const SOCIAL_LINKS = [
   { icon: Github, link: "https://github.com/billz1910", label: "GitHub Profile" },
   { icon: Linkedin, link: "https://www.linkedin.com/in/muhammad-nabil-sutisna-b98809332/", label: "LinkedIn Profile" },
@@ -188,7 +188,7 @@ const Home = () => {
                   <p className="text-base md:text-lg text-gray-400 max-w-xl leading-relaxed font-light"
                     data-aos="fade-up"
                     data-aos-delay="1000">
-                    Menciptakan Website Yang Inovatif, Fungsional, dan User-Friendly untuk Solusi Digital.
+                    Memiliki pengalaman praktis dalam mengidentifikasi kerentanan (vulnerability assessment), pembersihan malware, serta penguatan keamanan (hardening) infrastruktur VPS dan direktori web
                   </p>
 
                   {/* Tech Stack */}
